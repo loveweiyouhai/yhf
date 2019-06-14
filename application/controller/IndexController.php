9ec46f637c086612c3f4db52e5285ac54409870a
weiyouhai <weiyouhai2008@163.com>
2019-06-15 01:03:19 +0800

    修改控制器

--- application/controller/IndexController.php
+++ application/controller/IndexController.php
@@ -18,35 +18,5 @@ class IndexController extends Controller
     public function index()
     {
         $this->display("index");
-        $session = new Session();
-        dump($session::getAll());
     }
-
-    public function capth()
-    {
-        $builder = new CaptchaBuilder();
-        $builder->build();
-        header('Content-type: image/jpeg');
-        $builder->output();
-    }
-
-    public function sess()
-    {
-        $session = new Session();
-        $session::set('name', 'admin');
-        dump($session::get('name'));
-    }
-    public function sess1()
-    {
-        $session = new Session();
-        $session::set('age', '10');
-        dump($session::get('age'));
-    }
-    public function sess2()
-    {
-        $session = new Session();
-        $session::set('birthday', '29');
-        dump($session::get('birthday'));
-    }
-
 }
